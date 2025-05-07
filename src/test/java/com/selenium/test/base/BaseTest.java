68e33c0f6f264669d92568b5a5136b4c7f9c7ab2
ORAU <heymandameunmail@gmail.com>
2025-05-07 15:02:25 -0600

    Se agregó método en clase BaseTest para ejecutar test desde selenium Grid

--- src/test/java/com/selenium/test/base/BaseTest.java
+++ src/test/java/com/selenium/test/base/BaseTest.java
@@ -1,10 +1,17 @@
 package com.selenium.test.base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public abstract class BaseTest {
 
@@ -12,11 +19,42 @@ public abstract class BaseTest {
 
     @BeforeTest
     public void setup(){
-        WebDriverManager.chromedriver().setup();
-        this.driver=new ChromeDriver();
+
+        if(Boolean.getBoolean("selenium.grid.enabled")){
+            this.driver=remoteSetup();
+        }else{
+            this.driver=setupLocalWebdriver();
+        }
         this.driver.manage().window().maximize();
     }
 
+    /**
+     * Codigo para ejecutar selenium grid
+     * **/
+    private WebDriver remoteSetup()  {
+
+        Capabilities capabilities;
+        if(System.getProperty("browser").equalsIgnoreCase("chrome")){
+            capabilities=new ChromeOptions();
+        }
+        else{
+            capabilities=new FirefoxOptions();
+        }
+        try {
+            return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);
+
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /** Codigo para crear un Chrome Driver*/
+    private WebDriver setupLocalWebdriver(){
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        return driver;
+    }
     @AfterTest
     public void endingTest() {
 
